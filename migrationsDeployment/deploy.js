de4addc404345036e17ed7c723bb373b3ab3fc35
shreyd04 <officialsd27204@gmail.com>
2025-08-25 01:07:50 +0530

    .

--- migrationsDeployment/deploy.js
+++ migrationsDeployment/deploy.js
@@ -3,59 +3,58 @@ const path = require("path");
 const fs = require("fs");
 
 async function main() {
-  const [deployer] = await hre.ethers.getSigners();
+  const [deployer] = await hre.ethers.getSigners();// getting the metamask account of deployers here or ethereum account
   console.log("Deployer address:", deployer.address);
 
-  // Deploy IssuerRegistry
-  const IssuerRegistry = await hre.ethers.getContractFactory("IssuerRegistry");
+  const IssuerRegistry = await hre.ethers.getContractFactory("IssuerRegistry");// getContractfactory brings all the compiled contacts into issuer registry
   const issuerRegistry = await IssuerRegistry.deploy(deployer.address);
   await issuerRegistry.deployed();
   console.log("IssuerRegistry:", issuerRegistry.address);
 
-  // Deploy DIDRegistry
   const DIDRegistry = await hre.ethers.getContractFactory("DIDRegistry");
   const didRegistry = await DIDRegistry.deploy();
   await didRegistry.deployed();
   console.log("DIDRegistry:", didRegistry.address);
 
-  // Deploy CredentialRegistry
   const CredentialRegistry = await hre.ethers.getContractFactory("CredentialRegistry");
   const credentialRegistry = await CredentialRegistry.deploy(issuerRegistry.address);
   await credentialRegistry.deployed();
   console.log("CredentialRegistry:", credentialRegistry.address);
 
   // Add deployer as issuer
-  await (await issuerRegistry.addIssuer(deployer.address)).wait();
+  await (await issuerRegistry.addIssuer(deployer.address)).wait();// here making issuer the deployer
   console.log("Added deployer as issuer.");
 
-  // Save contract info
-  const contracts = {
-    issuerRegistry: {
-      address: issuerRegistry.address,
-      abi: JSON.parse(IssuerRegistry.interface.formatJson())
-    },
-    didRegistry: {
-      address: didRegistry.address,
-      abi: JSON.parse(DIDRegistry.interface.formatJson())
-    },
-    credentialRegistry: {
-      address: credentialRegistry.address,
-      abi: JSON.parse(CredentialRegistry.interface.formatJson())
-    }
-  };
+  // print addresses JSON for convenience
+  console.log(JSON.stringify({ // this is beautifully keeping the strings of all the required contracts 
+    issuerRegistry: issuerRegistry.address,
+    didRegistry: didRegistry.address,
+    credentialRegistry: credentialRegistry.address
+  }, null, 2));
 
-  const backendPath = path.join(__dirname, "../backend/src/config/contract.json");
-  const frontendPath = path.join(__dirname, "../frontend/src/config/contract.json");
-
-  fs.writeFileSync(backendPath, JSON.stringify(contracts, null, 2));
-  fs.writeFileSync(frontendPath, JSON.stringify(contracts, null, 2));
-
-  console.log("Contract ABIs and addresses saved to backend & frontend config.");
+// For actual frontend/backend interaction contracrt jason is required to combin the adress and ABI to tell how that contract will work
+ const contract = {
+  issuerRegistry : {
+    address : issuerRegistry.address,
+    abi : JSON.parse(IssuerRegistry.interface.formatJson())
+  },
+  didRegistry : {
+    address : didRegistry.address,
+    abi : JSON.parse(DIDRegistry.interface.formatJson())
+  },
+  credentialRegistry : {
+    address : credentialRegistry.address,
+    abi : JSON.parse(CredentialRegistry.interface.formatJson())
+  },
+ };
+ // saving the contract.json to backend and frontend
+ const backendPath = path.join(__dirname , "../backend/src/config/contract.json");
+ const frontendPath = path.join(__dirname , "../frontend/src/config/contract.json");
+ 
+ fs.writeFileSync(backendPath , JSONS.stringfy(contracts , null ,2));
+ fs.writeFileSync(frontendPath ,JSON.stringfy(contracts , null , 2));
 }
-
-// Run main
-main().catch((err) => {
+main().catch((err) => { // this is for the error , if any error then this msg will be printed
   console.error(err);
   process.exitCode = 1;
 });
-
