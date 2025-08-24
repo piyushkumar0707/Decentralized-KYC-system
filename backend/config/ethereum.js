277f9f5762062182d7acf0e6917680732428e889
shreyd04 <officialsd27204@gmail.com>
2025-08-24 11:48:19 +0530

    on ethereum.js

--- backend/config/ethereum.js
+++ backend/config/ethereum.js
@@ -1,6 +1,11 @@
 const {ethers} = require("ethers");
 const contracts = require("./contracts.jason"); // genrates by deploy script
+const path = require("path");
 require("dotenv").config();
 
+const RPC_URL = process.env.RPC_URL;
+if(!RPC_URL){
+    throw new Error("Missing RPC_URL"); 
+}
 const provide = new ethers.JsonRpcProvider(process.env.RPC_URL);
 
