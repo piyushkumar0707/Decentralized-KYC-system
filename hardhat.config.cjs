7602f3f2e9600d1d876903a6e0098f78b1d11955
shreyd04 <officialsd27204@gmail.com>
2025-09-01 00:08:09 +0530

    .

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,18 +1,13 @@
-require ("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-
-module.exports =  {
+module.exports = {
   solidity: {
     version: "0.8.30",
     settings: { optimizer: { enabled: true, runs: 200 } }
   },
   networks: {
     hardhat: {},
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",//let's you talk your hardhat project with the blockchain
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    },
     polygon: {
       url: process.env.POLYGON_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
